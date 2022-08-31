b2f880424dcc9a35220250c198e6474fb62483d3
Robert Seidl <robertseidl425@gmail.com>
2022-08-31 22:49:51 +0200

    added code to nb

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://seidlr.github.io',
                 'git_url': 'https://github.com/seidlr/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': { 'nbdev_hello_world.core.bar': ('core.html#bar', 'nbdev_hello_world/core.py'),
+                                        'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
